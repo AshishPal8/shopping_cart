4e8a1cba73cd2e10e129457390f20fb8b94bdb9c
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2024-03-14 08:05:29 +0530

    implement load more button

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,13 +6,18 @@ import ProductCard from "./ProductCard";
 function Products() {
   const { isDarkMode } = useContext(DarkModeContext);
   const [products, setProducts] = useState([]);
-  const url = "https://dummyjson.com/products/";
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
+
+  const url = `https://dummyjson.com/products/?limit=21&skip=${
+    count === 0 ? 0 : count * 21
+  }`;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(url);
-        setProducts(response.data.products);
+        setProducts((prevData) => [...prevData, ...response.data.products]);
         console.log(response.data.products);
       } catch (error) {
         console.log("Error", error);
@@ -20,7 +25,11 @@ function Products() {
     };
 
     fetchData();
-  }, []);
+  }, [count]);
+
+  useEffect(() => {
+    if (products && products.length === 100) setDisableButton(true);
+  }, [products]);
 
   return (
     <div
@@ -32,6 +41,16 @@ function Products() {
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
+      </div>
+      <div className="flex justify-center items-center my-4">
+        <button
+          className="bg-[#40A2E3] py-2 px-4 rounded-lg text-white font-semibold text-xl"
+          onClick={() => setCount(count + 1)}
+          disabled={disableButton}
+        >
+          Load More
+        </button>
+        {disableButton ? <p>You reached 100 products limit</p> : null}
       </div>
     </div>
   );
